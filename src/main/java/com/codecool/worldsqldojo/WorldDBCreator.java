d07ee529ac78e4131caa5e3bb4114cf8051e29b2
Szilveszter Erdős <szilveszter.erdos@codecool.com>
2017-10-04 12:31:13 +0200

    tests for checking table contents & necessary updates in main class

--- src/main/java/com/codecool/worldsqldojo/WorldDBCreator.java
+++ src/main/java/com/codecool/worldsqldojo/WorldDBCreator.java
@@ -1,5 +1,14 @@
 package com.codecool.worldsqldojo;
 
+import org.postgresql.copy.CopyManager;
+import org.postgresql.core.BaseConnection;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.*;
 
 public class WorldDBCreator {
@@ -7,7 +16,9 @@ public class WorldDBCreator {
     private static final String DB_USER = System.getenv("POSTGRES_DB_USER");
     private static final String DB_PASSWORD = System.getenv("POSTGRES_DB_PASSWORD");
 
-    public PreparedStatement getPreparedStatement(String query) {
+    public WorldDBCreator() {
+        executeUpdateFromFile("src/main/resources/init_db.sql");
+    }
 
     public Connection getConnection() {
         try {
@@ -43,5 +54,44 @@ public class WorldDBCreator {
             System.err.println("ERROR: SQL Timeout");
         }
         return null;
+    }
+
+    public void copyDataFromFile(String tableName, String filePath) throws SQLException {
+        FileReader fileReader = null;
+
+        try (Connection connection = getConnection()) {
+
+            try {
+                fileReader = new FileReader(filePath);
+            } catch (FileNotFoundException e) {
+                System.err.println("ERROR: File not found!");
+            }
+
+            CopyManager copyManager = new CopyManager((BaseConnection) connection);
+            try {
+                copyManager.copyIn("COPY " + tableName + " FROM STDIN", fileReader);
+            } catch (IOException e) {
+                System.err.println("ERROR: IO Error occured!");
+                e.printStackTrace();
+            }
+
+        } catch (SQLTimeoutException e) {
+            System.err.println("ERROR: SQL Timeout");
+        }
+    }
+
+    public void executeUpdateFromFile(String filePath) {
+        String query = "";
+        try {
+            query = new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
