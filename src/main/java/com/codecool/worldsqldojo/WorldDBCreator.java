685dd63d5636cf52fb22f02848e61f989f46ba5e
Szilveszter Erdős <szilveszter.erdos@codecool.com>
2017-10-04 08:01:26 +0200

    fix connection closing error due to calling executeQuery outside try-with-resource block

--- src/main/java/com/codecool/worldsqldojo/WorldDBCreator.java
+++ src/main/java/com/codecool/worldsqldojo/WorldDBCreator.java
@@ -8,19 +8,22 @@ public class WorldDBCreator {
     private static final String DB_PASSWORD = System.getenv("POSTGRES_DB_PASSWORD");
 
     public PreparedStatement getPreparedStatement(String query) {
-        try (Connection conn =
-             DriverManager.getConnection(DATABASE,
-                                         DB_USER,
-                                         DB_PASSWORD)){
-            return conn.prepareStatement(query);
-        } catch (SQLTimeoutException e) {
-            System.err.println("ERROR: SQL Timeout");
-        } catch (SQLException e) {
-        }
+
         return null;
     }
 
-    public ResultSet executeQuery(PreparedStatement statement) throws SQLException {
-        return statement.executeQuery();
+    public ResultSet executeQuery(String query) throws SQLException {
+        try (Connection connection = DriverManager.getConnection(
+                DATABASE,
+                DB_USER,
+                DB_PASSWORD)) {
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            return statement.executeQuery();
+
+        } catch (SQLTimeoutException e) {
+            System.err.println("ERROR: SQL Timeout");
+        }
+        return null;
     }
 }
